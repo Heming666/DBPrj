7321ebbd27b74450e5933c664b74cc97785aab4c
明 何 <hm80@vip.qq.com>
2021-02-26 11:01:52 +0800

    132456

---
+++ MyPrj/DoMain/UserManual.cs
@@ -0,0 +1,17 @@
+﻿using Common.Models;
+using Repository;
+
+namespace MyPrj.DoMain
+{
+    public class UserManual
+    {
+        private readonly IRepository _repository;
+        private readonly DBModel _dbModel;
+
+        public UserManual(IRepository repository, DBModel dbModel)
+        {
+            _repository = repository;
+            _dbModel = dbModel;
+        }
+    }
+}
